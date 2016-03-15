1d3871609c8ca21a63e5c9117f9f642ed3efa652
christopher snow <chsnow123@gmail.com>
2016-03-15 17:56:49 +0000

    add TODOs

--- examples/BigR/test.r
+++ examples/BigR/test.r
@@ -17,6 +17,26 @@ hostname <- Sys.getenv("hostname")
 username <- Sys.getenv("username")
 password <- Sys.getenv("password")
 
+print(hostname)
+
 library(bigr)
 bigr.connect(host=hostname, user=username, password=password)
+
+# FIXME: we should probaby connect using SSL
+# 
+# 1. Need to add the certificate alongside connection.properties
+# 2. Need this automating:
+#
+# keytool -import -trustcacerts -alias biginsights -file  ../../ehaasp-10451-mastermanager.bi.services.bluemix.net -keystore ./truststore.jks -storepass mypassword -noprompt
+#
+# bigr.connect(
+#     host=hostname, 
+#     user=username,
+#     password=password,
+#     ssl = TRUE,
+#     trustStorePath = "./truststore.jks",
+#     trustStorePassword = "mypassword2",
+#     keyManager = "SunX509"
+#     )
+
 is.bigr.connected()  
