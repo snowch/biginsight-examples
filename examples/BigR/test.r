1fe87408fa297417bedc7dbb719a544f3391692f
christopher snow <chsnow123@gmail.com>
2016-03-15 14:14:53 +0000

    add initial BigR project

---
+++ examples/BigR/test.r
@@ -0,0 +1,14 @@
+dir.create('./lib')
+
+install.packages('rJava', repos='http://cran.us.r-project.org', lib='./lib', quiet=FALSE)
+install.packages('base64enc', repos='http://cran.us.r-project.org', lib='./lib', quiet=FALSE)
+install.packages('data.table', repos='http://cran.us.r-project.org', lib='./lib', quiet=FALSE)
+install.packages('bigr.tar.gz', type="source", repos=NULL, lib='./lib', quiet=FALSE)
+
+hostname <- Sys.getenv("hostname")
+username <- Sys.getenv("username")
+password <- Sys.getenv("password")
+
+library(bigr)
+bigr.connect(host=hostname, user=username, password=password)
+is.bigr.connected()  
