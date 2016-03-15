697271c0e297eaea156ae7093d63fd3be5925395
christopher snow <chsnow123@gmail.com>
2016-03-15 20:46:01 +0000

    improve bigsql connectivity test

--- examples/BigSQL/Example.groovy
+++ examples/BigSQL/Example.groovy
@@ -31,6 +31,5 @@ sql = Sql.newInstance(db.url, db.user, db.password, db.driver)
 
 // test connectivity
 
-sql.eachRow("SELECT 'connected' FROM SYSIBM.SYSDUMMY1") { row ->
-     println "${row}"
- }
+def rows = sql.rows("SELECT 'connected' FROM SYSIBM.SYSDUMMY1")
+assert rows.size() == 1
