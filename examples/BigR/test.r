639bda731cf5783757fc2271b91deac67b169dca
christopher snow <chsnow123@gmail.com>
2016-03-15 16:39:17 +0000

    added clean target

--- examples/BigR/test.r
+++ examples/BigR/test.r
@@ -1,9 +1,15 @@
-dir.create('./lib')
 
-install.packages('rJava', repos='http://cran.us.r-project.org', lib='./lib', quiet=FALSE)
-install.packages('base64enc', repos='http://cran.us.r-project.org', lib='./lib', quiet=FALSE)
-install.packages('data.table', repos='http://cran.us.r-project.org', lib='./lib', quiet=FALSE)
-install.packages('bigr.tar.gz', type="source", repos=NULL, lib='./lib', quiet=FALSE)
+if (!dir.exists('./lib')) {
+
+    # create directory to hold libraries
+    dir.create('./lib')
+
+    # install libraries
+    install.packages('rJava', repos='http://cran.us.r-project.org', lib='./lib', quiet=FALSE)
+    install.packages('base64enc', repos='http://cran.us.r-project.org', lib='./lib', quiet=FALSE)
+    install.packages('data.table', repos='http://cran.us.r-project.org', lib='./lib', quiet=FALSE)
+    install.packages('bigr.tar.gz', type="source", repos=NULL, lib='./lib', quiet=FALSE)
+}
 
 hostname <- Sys.getenv("hostname")
 username <- Sys.getenv("username")
