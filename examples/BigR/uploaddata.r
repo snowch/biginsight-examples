98b204e5ab50aaa0b2d7448a683096264a2af0dc
christopher snow <chsnow123@gmail.com>
2016-04-29 16:03:10 +0100

    verify data exists after uploading with bigr

--- examples/BigR/uploaddata.r
+++ examples/BigR/uploaddata.r
@@ -1,7 +1,7 @@
 projdir <- Sys.getenv("projdir")
 
+# connect.r will open the connection to the cluster
 source( paste( projdir, "/connect.r", sep="" ) )
-
 
 #################
 # 1. Data loading
@@ -13,6 +13,8 @@ source( paste( projdir, "/connect.r", sep="" ) )
 airfile <- system.file("extdata", "airline.zip", package="bigr")
 airfile <- unzip(airfile, exdir = tempdir())
 airR <- read.csv(airfile, stringsAsFactors=F)
+
+# TODO: use a randomly generate name for the csv - we don't want to clobber someone elase's data.
 
 # Upload the data to the BigInsights server. This may take 15-20 seconds
 air <- as.bigr.frame(airR)
@@ -33,6 +35,12 @@ air_hdfs <- bigr.frame(dataPath = "/user/bigr/examples/airline_demo.csv",
                   coltypes = ifelse(1:29 %in% c(9,11,17,18,23), "character", "integer"),
                   useMapReduce = F)
 
-bigr.listfs("/user/bigr/examples/airline_demo.csv")
+air_hdfs_rows <- nrow(air_hdfs)
 
+# verify we have saved some data to hdfs
+if (air_hdfs_rows != 128790) {
+    stop(paste("Expecting ", 128790, " rows, but found ", air_hdfs_rows, sep=""))
+}
+
+# clean up any changes this script has made
 bigr.rmfs("/user/bigr/examples/airline_demo.csv", force = F, recursive = T)
