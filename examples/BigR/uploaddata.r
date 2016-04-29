536f497d52af6cd1c0e66ba5069c5f16e68ab6ce
christopher snow <chsnow123@gmail.com>
2016-04-29 15:07:23 +0100

    add upload data example for bigr

---
+++ examples/BigR/uploaddata.r
@@ -0,0 +1,38 @@
+projdir <- Sys.getenv("projdir")
+
+source( paste( projdir, "/connect.r", sep="" ) )
+
+
+#################
+# 1. Data loading
+#################
+
+# In order to try out any example, first run the following steps to upload
+# the aforementioned dataset to a BigInsights cluster.
+
+airfile <- system.file("extdata", "airline.zip", package="bigr")
+airfile <- unzip(airfile, exdir = tempdir())
+airR <- read.csv(airfile, stringsAsFactors=F)
+
+# Upload the data to the BigInsights server. This may take 15-20 seconds
+air <- as.bigr.frame(airR)
+air <- bigr.persist(air, dataSource="DEL", 
+                         dataPath="/user/bigr/examples/airline_demo.csv",
+                         header=T, delimiter=",", useMapReduce=F)
+
+###########################
+# 2. Accessing data on HDFS
+###########################
+
+# Once uploaded, one merely needs to instantiate a big.frame object,
+# commonly referenced as "air" in the examples, to access the dataset via
+# the Big R API.
+air_hdfs <- bigr.frame(dataPath = "/user/bigr/examples/airline_demo.csv",
+                  dataSource = "DEL",
+                  delimiter=",", header = T,
+                  coltypes = ifelse(1:29 %in% c(9,11,17,18,23), "character", "integer"),
+                  useMapReduce = F)
+
+bigr.listfs("/user/bigr/examples/airline_demo.csv")
+
+bigr.rmfs("/user/bigr/examples/airline_demo.csv", force = F, recursive = T)
