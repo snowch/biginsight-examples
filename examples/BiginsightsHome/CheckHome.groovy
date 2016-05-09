2d2e3a8a185ec5b315d5ee8d865c2f272277ede2
Pierre R. Regazzoni <pierrer@us.ibm.com>
2016-05-09 10:41:57 -0700

    Add BI Home example
    relates to issue#69

---
+++ examples/BiginsightsHome/CheckHome.groovy
@@ -0,0 +1,42 @@
+import geb.Browser
+import org.openqa.selenium.firefox.FirefoxDriver
+
+def browser = new Browser(driver: new FirefoxDriver())
+
+env = System.getenv()
+
+def username = env.username
+def password = env.password
+def gateway = env.gateway
+
+def matcher = gateway =~ /^(https?:\/\/)([^:^\/]*)(:\d*)?(.*)?.*$/
+def host = matcher[0][2]
+def port = matcher[0][3]
+
+BiHome = "https://" + username + ":" + password + "@" + host + port + "/gateway/default/BigInsightsWeb/#/welcome"
+
+println "Opening BIHOME"
+println BiHome
+
+browser.drive {
+
+    go BiHome
+
+    // Check we enter BigInsights Home
+    assert title == "IBM BigInsights"
+
+    // Check for BigSheets box
+    println "Check BigSheets"
+    assert $("div", class: "comp-name ng-binding").eq(0).text() == "BigSheets"
+
+    println "Check for Text Analytics"
+    assert $("div", class: "comp-name ng-binding").eq(1).text() == "Text Analytics"
+
+    println "Check for Data Server Manager for Big SQL"
+    assert $("div", class: "comp-name ng-binding").eq(2).text() == "Data Server Manager for Big SQL"
+
+}
+
+browser.close()
+
+
